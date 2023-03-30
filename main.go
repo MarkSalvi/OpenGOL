2203ddd7b5ff1bb50f9b28e2c74b0ba6e447b7c5
MarkSalvi <mark.salvi.19@gmail.com>
2023-03-31 00:48:36 +0200

    sium3

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"log"
+	"math"
 	"runtime"
 	"strings"
 )
@@ -20,9 +21,10 @@ const (
 
 	fragmentShaderSource = `
     #version 330 core
+	uniform vec4 ourColor;
     out vec4 frag_colour;
     void main() {
-        frag_colour = vec4(1, 0.5, 0.2, 1);
+        frag_colour = ourColor;
     }
 ` + "\x00"
 )
@@ -34,13 +36,13 @@ func init() {
 func main() {
 
 	var triangle = []float32{
-		0.5, 0.5, 0.0, // top right
-		0.5, -0.5, 0.0, // bottom right
-		-0.5, -0.5, 0.0, // bottom left
+		0.5, 0.5, 0.0,
+		0.5, -0.5, 0.0,
+		-0.5, -0.5, 0.0,
 		-0.5, 0.5, 0.0,
 	}
 
-	var indices = []uint{
+	var indices = []uint32{
 		0, 1, 3,
 		1, 2, 3,
 	}
@@ -81,24 +83,38 @@ func main() {
 	gl.AttachShader(prog, vertexShader)
 	gl.AttachShader(prog, fragmentShader)
 	gl.LinkProgram(prog)
+	gl.UseProgram(prog)
 
-	vao := makeVao(triangle, indices)
+	vao, vbo, ebo := makeVao(triangle, indices)
 
 	for !window.ShouldClose() {
 
 		draw(window, prog, vao)
 
 	}
-
+	gl.DeleteProgram(prog)
+	gl.DeleteVertexArrays(1, &vao)
+	gl.DeleteBuffers(1, &ebo)
+	gl.DeleteBuffers(1, &vbo)
 	gl.DeleteShader(vertexShader)
 	gl.DeleteShader(fragmentShader)
 
 }
 
 func draw(window *glfw.Window, program uint32, vao uint32) {
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+	processInput(window)
+
+	gl.ClearColor(0.2, 0.3, 0.3, 1.0)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
 
 	gl.UseProgram(program)
+
+	timeValue := glfw.GetTime()
+	greenValue := math.Sin(timeValue)/2 + 0.5
+	vertexColorLocation := gl.GetUniformLocation(program, gl.Str("ourColor"+"\x00"))
+	gl.Uniform4f(vertexColorLocation, 0, float32(greenValue), 0, 1)
+
 	gl.BindVertexArray(vao)
 	gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.Ptr(nil))
 
@@ -106,26 +122,25 @@ func draw(window *glfw.Window, program uint32, vao uint32) {
 	window.SwapBuffers()
 }
 
-func makeVao(points []float32, indices []uint) uint32 {
+func makeVao(points []float32, indices []uint32) (uint32, uint32, uint32) {
 	var vao uint32
+	var vbo uint32
+	var ebo uint32
 	gl.GenVertexArrays(1, &vao)
+	gl.GenBuffers(1, &ebo)
+	gl.GenBuffers(1, &vbo)
+
 	gl.BindVertexArray(vao)
 
-	var vbo uint32
-	gl.GenBuffers(1, &vbo)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
 
-	var ebo uint32
-	gl.GenBuffers(1, &ebo)
 	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
 	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, 4*len(indices), gl.Ptr(indices), gl.STATIC_DRAW)
 
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, gl.Ptr(nil))
 	gl.EnableVertexAttribArray(0)
-	return vao
+	return vao, vbo, ebo
 }
 
 func compileShader(source string, fragmentType uint32) (uint32, error) {
@@ -149,4 +164,14 @@ func compileShader(source string, fragmentType uint32) (uint32, error) {
 	}
 
 	return shader, nil
+}
+
+func processInput(window *glfw.Window) {
+	if window.GetKey(glfw.KeyEscape) == glfw.Press {
+		window.SetShouldClose(true)
+	} else if window.GetKey(glfw.KeyF1) == glfw.Press {
+		gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
+	} else if window.GetKey(glfw.KeyF2) == glfw.Press {
+		gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL)
+	}
 }
